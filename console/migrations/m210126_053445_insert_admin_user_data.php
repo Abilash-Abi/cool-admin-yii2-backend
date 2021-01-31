9f25c4da49031599c91d1e752706f85195b445ef
Abilash Abi <abilashvattamkulam@gmail.com>
2021-01-31 13:11:11 +0530

    Migration issues fixed

---
+++ console/migrations/m210126_053445_insert_admin_user_data.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201018_120935_insert_admin_user_data
+ */
+class m210126_053445_insert_admin_user_data extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%admin_users}}',[
+            'username'=>'admin@yopmail.com',
+            'password_hash'=>'$2y$13$9qmCsSOxiOr8EoGNhY6kfeW6KwrcsB8l0H17oCcoet9Q6AAe4NLLW',
+            'auth_key'=>'',
+            'email'=>'admin@yopmail.com', 
+            'status'=>'10',
+            'created_at'=>'12345678',
+            'updated_at'=>'12345678',
+            'role_id'=>'1',
+
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m201018_120935_insert_admin_user_data cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201018_120935_insert_admin_user_data cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
